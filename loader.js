708ef67d2aacdf0a5f90751aa6b16201ea0e7118
Xeway <xeway@protonmail.com>
2021-08-18 20:27:46 +0200

    Finished loading screen when page isn't loaded yet

---
+++ loader.js
@@ -0,0 +1,7 @@
+let loader = document.querySelector("#loader");
+let loaderEmojis = document.querySelector("#loader-emojis");
+let neumorphismBox = document.querySelector("#neumorphism-box");
+
+window.addEventListener("load", function(e) {
+    loader.style.display = "none";
+});
