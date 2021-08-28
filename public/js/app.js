ecf121e952dd7844ff7923883cd07e056c4d602b
Xeway <xeway@protonmail.com>
2021-08-28 18:00:11 +0200

    The app remember the last time city name and the last weather

--- public/js/app.js
+++ public/js/app.js
@@ -19,6 +19,99 @@ button.addEventListener('mouseout', (e) => {
     e.target.textContent = valueButton;
 });
 
+function listCookies() {
+    let theCookies = document.cookie.split('; ');
+    let cookiesArray = [];
+    theCookies.forEach(theCookie => {
+        cookiesArray.push(theCookie);
+    });
+    return cookiesArray;
+}
+
+function getCookieValue(cookie) {
+    let cookieSplit = cookie.split("=");
+    return cookieSplit[1];
+}
+
+function fillElements(data, isStringified) {
+    if(isStringified) {
+        data = JSON.parse(data);
+    }
+    
+    let date = new Date(data.current.last_updated);
+    let month = date;
+    month.setMonth(month.getMonth() + 1);
+
+    let lastUpdatedDateFr = (((date.getDate() < 10) ? '0' : '') + date.getDate()) + "/" + (((month.getMonth() < 10) ? '0' : '') + month.getMonth()) + "/" + date.getFullYear() + " à " + (((date.getHours() < 10) ? '0' : '') + date.getHours())  + ":" + (((date.getMinutes() < 10) ? '0' : '') + date.getMinutes());
+    let lastUpdatedDateEn = date.getFullYear() + "-" + (((month.getMonth() < 10) ? '0' : '') + month.getMonth()) + "-" + (((date.getDate() < 10) ? '0' : '') + date.getDate()) + " at " + (((date.getHours() < 10) ? '0' : '') + date.getHours())  + ":00";
+
+    document.querySelector("#name-city-span").innerHTML = data.location.name + ", " + data.location.region + ", " + data.location.country;
+    if(language === "fr") {
+        document.querySelector('#current-hour-span').innerHTML = lastUpdatedDateFr;
+    } else {
+        document.querySelector('#current-hour-span').innerHTML = lastUpdatedDateEn;
+    }
+
+    loaderApi.style.visibility = "hidden";
+
+    currentWeather.children[0].setAttribute("src", data.current.condition.icon);
+    currentWeather.children[1].innerHTML = data.current.condition.text;
+    currentWeather.children[2].children[0].innerHTML = "Température : " + data.current.temp_c + "°C";
+    currentWeather.children[2].children[1].innerHTML = "Ressenti : " + data.current.feelslike_c + "°C";
+
+    let fullDate = date.getFullYear() + "-" + (((month.getMonth() < 10) ? '0' : '') + month.getMonth()) + "-" + (((date.getDate() < 10) ? '0' : '') + date.getDate()) + " " + (((date.getHours() < 10) ? '0' : '') + date.getHours())  + ":00";
+    for(let j = 0 ; j < data.forecast.forecastday[0].hour.length ; j++) {
+        if(data.forecast.forecastday[0].hour[j].time == fullDate) {
+            startHour = data.forecast.forecastday[0].hour[j];
+        }
+    }
+
+    startHour = new Date(startHour.time).getHours();
+    
+    todayWeather.children[0].innerHTML = data.forecast.forecastday[0].day.condition.text;
+    tomorrowWeather.children[0].innerHTML = data.forecast.forecastday[1].day.condition.text;
+    afterTomorrowWeather.children[0].innerHTML = data.forecast.forecastday[2].day.condition.text;
+
+    let hoursArray = [];
+    for(let k = 0, m = 0 ; k < data.forecast.forecastday[0].hour.length ; k++, m += 3) {
+        if(startHour + m < data.forecast.forecastday[0].hour.length && todayWeather.children[1].children[k] != undefined) {
+            todayWeather.children[1].children[k].children[0].innerHTML = new Date(data.forecast.forecastday[0].hour[startHour + m].time).getHours() + ":00";
+            todayWeather.children[1].children[k].children[1].setAttribute("src", data.forecast.forecastday[0].hour[startHour + m].condition.icon);
+            todayWeather.children[1].children[k].children[2].innerHTML = data.forecast.forecastday[0].hour[startHour + m].temp_c + "°C";
+            hoursArray.push(startHour + m);
+        }
+    }
+
+    let lastHour = hoursArray[hoursArray.length - 1];
+
+    let startTomorrowHour = (lastHour + 3) - 24;
+
+    for(let r = 0 ; r < weatherBoxesToday.children.length ; r++) {
+        if(weatherBoxesToday.children[r].children[0].textContent === "" && weatherBoxesToday.children[r].children[1].currentSrc === "" && weatherBoxesToday.children[r].children[2].textContent === "") {
+            if(data.forecast.forecastday[1].hour[startTomorrowHour] != undefined) {
+                todayWeather.children[1].children[r].children[0].innerHTML = new Date(data.forecast.forecastday[1].hour[startTomorrowHour].time).getHours() + ":00";
+                todayWeather.children[1].children[r].children[1].setAttribute("src", data.forecast.forecastday[1].hour[startTomorrowHour].condition.icon);
+                todayWeather.children[1].children[r].children[2].innerHTML = data.forecast.forecastday[1].hour[startTomorrowHour].temp_c + "°C";
+                startTomorrowHour += 3;
+            }
+        }
+    }
+
+    for(let n = 1 ; n < data.forecast.forecastday.length ; n++) {
+        for(let p = 0, q = 0 ; p < data.forecast.forecastday[n].hour.length ; p += 3, q++) {
+            if(n == 1) {
+                tomorrowWeather.children[1].children[q].children[0].innerHTML = new Date(data.forecast.forecastday[n].hour[p].time).getHours() + ":00";
+                tomorrowWeather.children[1].children[q].children[1].setAttribute("src", data.forecast.forecastday[n].hour[p].condition.icon);
+                tomorrowWeather.children[1].children[q].children[2].innerHTML = data.forecast.forecastday[n].hour[p].temp_c + "°C";
+            } else if (n == 2) {
+                afterTomorrowWeather.children[1].children[q].children[0].innerHTML = new Date(data.forecast.forecastday[n].hour[p].time).getHours() + ":00";
+                afterTomorrowWeather.children[1].children[q].children[1].setAttribute("src", data.forecast.forecastday[n].hour[p].condition.icon);
+                afterTomorrowWeather.children[1].children[q].children[2].innerHTML = data.forecast.forecastday[n].hour[p].temp_c + "°C";
+            }
+        }
+    }
+}
+
 button.addEventListener('click', (e) => {
     e.preventDefault();
     loaderApi.style.visibility = "visible";
@@ -33,76 +126,10 @@ button.addEventListener('click', (e) => {
             }
         })
         .then((value) => {
+            document.cookie = `city=${city.value}; max-age=${365*24*3600}; samesite=strict`;
+            localStorage.setItem("weather", JSON.stringify(value));
             document.querySelector('section').style.display = "block";
-            let date = new Date().getHours() + ":00";
-            let lastUpdatedDate = (((new Date(value.current.last_updated).getHours() < 10) ? '0' : '') + new Date(value.current.last_updated).getHours())  + ":" + (((new Date(value.current.last_updated).getMinutes() < 10) ? '0' : '') + new Date(value.current.last_updated).getMinutes());
-
-            document.querySelector("#name-city-span").innerHTML = value.location.name + ", " + value.location.region + ", " + value.location.country;
-            document.querySelector('#current-hour-span').innerHTML = lastUpdatedDate;
-
-            loaderApi.style.visibility = "hidden";
-
-            currentWeather.children[0].setAttribute("src", value.current.condition.icon);
-            currentWeather.children[1].innerHTML = value.current.condition.text;
-            currentWeather.children[2].children[0].innerHTML = "Température : " + value.current.temp_c + "°C";
-            currentWeather.children[2].children[1].innerHTML = "Ressenti : " + value.current.feelslike_c + "°C";
-
-            let month = new Date(value.current.last_updated);
-            month.setMonth(month.getMonth() + 1);
-
-            let fullDate = new Date(value.current.last_updated).getFullYear() + "-" + (((month.getMonth() < 10) ? '0' : '') + month.getMonth()) + "-" + (((new Date(value.current.last_updated).getDate() < 10) ? '0' : '') + new Date(value.current.last_updated).getDate()) + " " + (((new Date(value.current.last_updated).getHours() < 10) ? '0' : '') + new Date(value.current.last_updated).getHours())  + ":00";
-            
-            for(let j = 0 ; j < value.forecast.forecastday[0].hour.length ; j++) {
-                if(value.forecast.forecastday[0].hour[j].time == fullDate) {
-                    startHour = value.forecast.forecastday[0].hour[j];
-                }
-            }
-
-            startHour = new Date(startHour.time).getHours();
-            
-            todayWeather.children[0].innerHTML = value.forecast.forecastday[0].day.condition.text;
-            tomorrowWeather.children[0].innerHTML = value.forecast.forecastday[1].day.condition.text;
-            afterTomorrowWeather.children[0].innerHTML = value.forecast.forecastday[2].day.condition.text;
-
-            let hoursArray = [];
-            for(let k = 0, m = 0 ; k < value.forecast.forecastday[0].hour.length ; k++, m += 3) {
-                if(startHour + m < value.forecast.forecastday[0].hour.length && todayWeather.children[1].children[k] != undefined) {
-                    todayWeather.children[1].children[k].children[0].innerHTML = new Date(value.forecast.forecastday[0].hour[startHour + m].time).getHours() + ":00";
-                    todayWeather.children[1].children[k].children[1].setAttribute("src", value.forecast.forecastday[0].hour[startHour + m].condition.icon);
-                    todayWeather.children[1].children[k].children[2].innerHTML = value.forecast.forecastday[0].hour[startHour + m].temp_c + "°C";
-                    hoursArray.push(startHour + m);
-                }
-            }
-
-            let lastHour = hoursArray[hoursArray.length - 1];
-
-            let startTomorrowHour = (lastHour + 3) - 24;
-
-            for(let r = 0 ; r < weatherBoxesToday.children.length ; r++) {
-                if(weatherBoxesToday.children[r].children[0].textContent === "" && weatherBoxesToday.children[r].children[1].currentSrc === "" && weatherBoxesToday.children[r].children[2].textContent === "") {
-                    if(value.forecast.forecastday[1].hour[startTomorrowHour] != undefined) {
-                        todayWeather.children[1].children[r].children[0].innerHTML = new Date(value.forecast.forecastday[1].hour[startTomorrowHour].time).getHours() + ":00";
-                        todayWeather.children[1].children[r].children[1].setAttribute("src", value.forecast.forecastday[1].hour[startTomorrowHour].condition.icon);
-                        todayWeather.children[1].children[r].children[2].innerHTML = value.forecast.forecastday[1].hour[startTomorrowHour].temp_c + "°C";
-                        startTomorrowHour += 3;
-                    }
-                }
-            }
-
-            for(let n = 1 ; n < value.forecast.forecastday.length ; n++) {
-                for(let p = 0, q = 0 ; p < value.forecast.forecastday[n].hour.length ; p += 3, q++) {
-                    if(n == 1) {
-                        tomorrowWeather.children[1].children[q].children[0].innerHTML = new Date(value.forecast.forecastday[n].hour[p].time).getHours() + ":00";
-                        tomorrowWeather.children[1].children[q].children[1].setAttribute("src", value.forecast.forecastday[n].hour[p].condition.icon);
-                        tomorrowWeather.children[1].children[q].children[2].innerHTML = value.forecast.forecastday[n].hour[p].temp_c + "°C";
-                    } else if (n == 2) {
-                        afterTomorrowWeather.children[1].children[q].children[0].innerHTML = new Date(value.forecast.forecastday[n].hour[p].time).getHours() + ":00";
-                        afterTomorrowWeather.children[1].children[q].children[1].setAttribute("src", value.forecast.forecastday[n].hour[p].condition.icon);
-                        afterTomorrowWeather.children[1].children[q].children[2].innerHTML = value.forecast.forecastday[n].hour[p].temp_c + "°C";
-                    }
-                }
-            }
-
+            fillElements(value, false);
         })
         .catch((err) => {
             document.querySelector('section').style.display = "none";
@@ -115,3 +142,12 @@ button.addEventListener('click', (e) => {
             }
         });
 });
+
+if(listCookies()[1] !== undefined) {
+    city.value = getCookieValue(listCookies()[1]);
+}
+
+if(localStorage.getItem("weather") !== null) {
+    document.querySelector('section').style.display = "block";
+    fillElements(localStorage.getItem("weather"), true);
+}
